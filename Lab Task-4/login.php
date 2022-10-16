11e5edd0f326733f2d5dc1e5a025fe1a3d466fdb
Muzahid <20-43841-2>
2022-10-16 16:45:41 +0600

    Lab Task 4

--- Lab Task-4/login.php
+++ Lab Task-4/login.php
@@ -1,14 +1,12 @@
 <?php
     session_start();
-
+    header('Location:Home.html');
     if (isset($_POST['submit'])) {
       $username = $_POST['username'];
       $password = $_POST['password'];
       if ($username == 'John' && $password == '12345678') {
 
         $_SESSION['username'] = $username;
-
-        header('Location:Home.html');
       } else {
         echo 'Incorrect username or password';
       }
