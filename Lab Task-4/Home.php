0daeda03773c5a1cdcec91be4f92160bedb0aec0
Mubashir Mohsin <mubashir.mohsin.42884@gmail.com>
2022-11-11 02:37:48 +0600

    Mid Labworks with Screenshots(11.11.2022)

--- Lab Task-4/Home.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <title>Homepage</title>
-</head>
-<body>
-    <h1 align="center">WELCOME <?php $_POST['username']; ?></h1>
-    <a href="logout.php">Log Out</a>
-</body>
-</html>
